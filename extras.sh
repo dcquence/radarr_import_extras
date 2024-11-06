0f9c2dbed7f572fb9e064fb6b833baafec3f5ded
Antibios <dcquence@gmail.com>
2024-11-05 19:17:36 -0600

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -2,11 +2,10 @@
 [[ "$radarr_eventtype" == "Test" ]] && exit 0
 
 cp -R "$radarr_moviefile_sourcepath" "$radarr_movie_path"
-cp -R "$radarr_moviefile_sourcefolder/Featurettes" "$radarr_movie_path/Extras" &&
 
-# Check if Featurettes folder exists and move it if present
-if test -d "$radarr_movie_path/Featurettes"; then
-    mv "$radarr_movie_path/Featurettes" "$radarr_movie_path/Extras"
+# Check if Featurettes folder exists and copy it if found
+if test -d "$radarr_moviefile_sourcefolder/Featurettes"; then
+   cp -R "$radarr_moviefile_sourcefolder/Featurettes" "$radarr_movie_path/Extras"
 fi
 
 # Check if Extras folder exists and create .nomedia file
